46bcc67e922e529631bb1bb76572ff18b69a753c
Goomba <milan.raymakers@gmail.com>
2022-01-02 13:51:03 +0100

    Create mbr.asm

---
+++ mbr.asm
@@ -0,0 +1,30 @@
+BITS 16
+ORG     0x7c00     
+jmp start
+ 
+start:
+        mov ax,cs
+        mov ds,ax
+        mov si,msg         
+        call print
+ 
+print:
+        push ax
+        cld
+next:
+        mov al,[si]
+        cmp al,0          
+        je done           
+        call printchar
+        inc si              
+        jmp next           
+done:
+        jmp $               
+ 
+printchar:
+        mov ah,0x0e
+        int 0x10
+        ret  
+msg:            db        "And once the computer reboots, Systemp has been initiated. Made with <3 by Imusing", 0
+times 510 - ($-$$) db 0
+dw        0xaa55
